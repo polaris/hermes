ab7a0b1942806249b88b455c3aa995e94888ecb0
Jan Deinhard <jan.deinhard@gmail.com>
2016-04-08 21:57:34 +0200

    improve tests for Buffer<>

--- test/BufferTest.cpp
+++ test/BufferTest.cpp
@@ -6,29 +6,29 @@ TEST_CASE("Buffer has correct default values", "[Buffer]") {
     REQUIRE(buffer.capacity() == 8);
 }
 
-/*TEST_CASE("Buffer can be read and written", "[Buffer]") {
+TEST_CASE("Buffer can be read and written", "[Buffer]") {
     Buffer<8> buffer;
 
     const uint32_t value = 0x12345678;
-    REQUIRE_NOTHROW(buffer.write(reinterpret_cast<const uint8_t*>(&value) + 0, 0, sizeof(uint8_t)));
-    REQUIRE_NOTHROW(buffer.write(reinterpret_cast<const uint8_t*>(&value) + 1, 1, sizeof(uint8_t)));
-    REQUIRE_NOTHROW(buffer.write(reinterpret_cast<const uint8_t*>(&value) + 2, 2, sizeof(uint8_t)));
-    REQUIRE_NOTHROW(buffer.write(reinterpret_cast<const uint8_t*>(&value) + 3, 3, sizeof(uint8_t)));
+    REQUIRE_NOTHROW(buffer.write(reinterpret_cast<const uint8_t*>(&value) + 0, sizeof(uint8_t)));
+    REQUIRE_NOTHROW(buffer.write(reinterpret_cast<const uint8_t*>(&value) + 1, sizeof(uint8_t)));
+    REQUIRE_NOTHROW(buffer.write(reinterpret_cast<const uint8_t*>(&value) + 2, sizeof(uint8_t)));
+    REQUIRE_NOTHROW(buffer.write(reinterpret_cast<const uint8_t*>(&value) + 3, sizeof(uint8_t)));
+
+    buffer.reset();
 
     uint32_t output{};
-    REQUIRE_NOTHROW(buffer.read(&output, 0, sizeof(uint32_t)));
+    REQUIRE_NOTHROW(buffer.read(&output, sizeof(uint32_t)));
 
     REQUIRE(output == value);
 }
 
 TEST_CASE("Exception is thrown when writing out of bounds", "[Buffer]") {
-    Buffer<4> buffer;
+    Buffer<2> buffer;
 
     uint32_t value = 0;
-    REQUIRE_THROWS_AS(buffer.write(&value, 1, sizeof(uint32_t)), std::out_of_range);
-    REQUIRE_THROWS_AS(buffer.write(&value, 2, sizeof(uint32_t)), std::out_of_range);
-    REQUIRE_THROWS_AS(buffer.write(&value, 3, sizeof(uint32_t)), std::out_of_range);
-}*/
+    REQUIRE_THROWS_AS(buffer.write(&value, sizeof(uint32_t)), std::out_of_range);
+}
 
 TEST_CASE("BufferReader can be read", "[BufferReader]") {
     Buffer<32> buffer;
