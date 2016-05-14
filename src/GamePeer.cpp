963bfbfd2bd974649bf9ed6f42081a43befee1fc
Jan Deinhard <jan.deinhard@gmail.com>
2016-05-14 22:13:50 +0200

    work in progress on peer-to-peer RTS demo

--- src/GamePeer.cpp
+++ src/GamePeer.cpp
@@ -2,11 +2,20 @@
 #include "Renderer.h"
 
 GamePeer::GamePeer(unsigned int frameRate, Renderer& renderer, unsigned short port)
-: Game(frameRate, renderer) {
+: GamePeer(frameRate, renderer, nullptr, port) {
+    currentState.reset(new GamePeer::Accepting{this});
 }
 
 GamePeer::GamePeer(unsigned int frameRate, Renderer& renderer, const char* masterAddress, unsigned short port)
-: Game(frameRate, renderer) {
+: Game(frameRate, renderer)
+, inputHandler_(30)
+, latencyEstimator_(10)
+, currentState(nullptr)
+, nextState(nullptr)
+, bufferedQueue_(1000)
+, latencyEmulator_(bufferedQueue_, 150)
+, transceiver_(latencyEmulator_) {
+    currentState.reset(new GamePeer::Connecting{this});
 }
 
 void GamePeer::handleWillUpdateWorld(const Clock&) {
@@ -24,11 +33,11 @@ void GamePeer::handleEvent(SDL_Event &event, bool& running) {
         if (event.key.keysym.sym == SDLK_ESCAPE) {
             running = false;
         } else {
-            //inputHandler_.handleInput(KeyAction::Down, event.key.keysym.sym);
+            inputHandler_.handleInput(KeyAction::Down, event.key.keysym.sym);
         }
         break;
     case SDL_KEYUP:
-        //inputHandler_.handleInput(KeyAction::Up, event.key.keysym.sym);
+        inputHandler_.handleInput(KeyAction::Up, event.key.keysym.sym);
         break;
     default:
         break;
@@ -36,4 +45,72 @@ void GamePeer::handleEvent(SDL_Event &event, bool& running) {
 }
 
 void GamePeer::finishFrame() {
+}
+
+GamePeer::State::State(GamePeer* gamePeer)
+: gamePeer_(gamePeer) {
+}
+
+GamePeer::Accepting::Accepting(GamePeer* gamePeer)
+: State(gamePeer) {
+}
+
+void GamePeer::Accepting::handleWillUpdateWorld(const Clock&) {
+
+}
+
+void GamePeer::Accepting::handleIncomingPacket(Packet* packet, const Clock& clock) {
+
+}
+
+void GamePeer::Accepting::sendOutgoingPackets(const Clock& clock) {
+
+}
+
+GamePeer::Connecting::Connecting(GamePeer* gamePeer)
+: State(gamePeer) {
+}
+
+void GamePeer::Connecting::handleWillUpdateWorld(const Clock&) {
+
+}
+
+void GamePeer::Connecting::handleIncomingPacket(Packet* packet, const Clock& clock) {
+
+}
+
+void GamePeer::Connecting::sendOutgoingPackets(const Clock& clock) {
+
+}
+
+GamePeer::Waiting::Waiting(GamePeer* gamePeer)
+: State(gamePeer) {
+}
+
+void GamePeer::Waiting::handleWillUpdateWorld(const Clock&) {
+
+}
+
+void GamePeer::Waiting::handleIncomingPacket(Packet* packet, const Clock& clock) {
+
+}
+
+void GamePeer::Waiting::sendOutgoingPackets(const Clock& clock) {
+
+}
+
+GamePeer::Playing::Playing(GamePeer* gamePeer)
+: State(gamePeer) {
+}
+
+void GamePeer::Playing::handleWillUpdateWorld(const Clock&) {
+
+}
+
+void GamePeer::Playing::handleIncomingPacket(Packet* packet, const Clock& clock) {
+
+}
+
+void GamePeer::Playing::sendOutgoingPackets(const Clock& clock) {
+
 }
--- src/GamePeer.h
+++ src/GamePeer.h
@@ -3,6 +3,12 @@
 
 #include "Game.h"
 #include "InputHandler.h"
+#include "LatencyEstimator.h"
+#include "BufferedQueue.h"
+#include "LatencyEmulator.h"
+#include "Transceiver.h"
+
+#include <memory>
 
 class GamePeer : public Game {
 public:
@@ -12,12 +18,69 @@ public:
 
 private:
     void handleWillUpdateWorld(const Clock& clock) override;
-
     void handleDidUpdateWorld(const Clock& clock) override;
-
     void handleEvent(SDL_Event &event, bool& running) override;
-
     void finishFrame() override;
+
+    class State {
+    public:
+        explicit State(GamePeer* gamePeer);
+        virtual ~State() = default;
+        State(const GamePeer::State&) = delete;
+        GamePeer::State& operator =(const GamePeer::State&) = delete;
+        virtual void handleWillUpdateWorld(const Clock&) {}
+        virtual void handleIncomingPacket(Packet* packet, const Clock& clock) = 0;
+        virtual void sendOutgoingPackets(const Clock& clock) = 0;
+
+    protected:
+        GamePeer* gamePeer_;
+    };
+
+    using StatePtr = std::shared_ptr<State>;
+
+    void setState(StatePtr& newState);
+
+    class Accepting : public State {
+    public:
+        explicit Accepting(GamePeer* gamePeer);
+        void handleWillUpdateWorld(const Clock&) override;
+        void handleIncomingPacket(Packet* packet, const Clock& clock) override;
+        void sendOutgoingPackets(const Clock& clock) override;
+    };
+
+    class Connecting : public State {
+    public:
+        explicit Connecting(GamePeer* gamePeer);
+        void handleWillUpdateWorld(const Clock&) override;
+        void handleIncomingPacket(Packet* packet, const Clock& clock) override;
+        void sendOutgoingPackets(const Clock& clock) override;
+    };
+
+    class Waiting : public State {
+    public:
+        explicit Waiting(GamePeer* gamePeer);
+        void handleWillUpdateWorld(const Clock&) override;
+        void handleIncomingPacket(Packet* packet, const Clock& clock) override;
+        void sendOutgoingPackets(const Clock& clock) override;
+    };
+
+    class Playing : public State {
+    public:
+        explicit Playing(GamePeer* gamePeer);
+        void handleWillUpdateWorld(const Clock&) override;
+        void handleIncomingPacket(Packet* packet, const Clock& clock) override;
+        void sendOutgoingPackets(const Clock& clock) override;
+    };
+
+    InputHandler inputHandler_;
+    LatencyEstimator latencyEstimator_;
+
+    StatePtr currentState;
+    StatePtr nextState;
+
+    BufferedQueue bufferedQueue_;
+    LatencyEmulator latencyEmulator_;
+    Transceiver transceiver_;
 };
 
 #endif	// _GamePeer_H
