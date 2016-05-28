ac71cc6c72e4610ff4730492776b7e692369d92d
Jan Deinhard <jan.deinhard@gmail.com>
2016-05-28 23:18:17 +0200

    collision detection work in progress

--- src/ServerWorld.cpp
+++ src/ServerWorld.cpp
@@ -3,8 +3,10 @@
 #include "SpaceShip.h"
 #include "Logging.h"
 
+#include <boost/format.hpp>
+
 #include <unordered_set>
-#include <tuple>
+#include <string>
 
 ServerWorld::ServerWorld(unsigned int width, unsigned int height) 
 : World()
@@ -15,7 +17,7 @@ ServerWorld::ServerWorld(unsigned int width, unsigned int height)
 void ServerWorld::update(float elapsed) {
     World::update(elapsed);
 
-    std::unordered_set<std::tuple<uint32_t, uint32_t>> checked;
+    std::unordered_set<std::string> checked;
 
     forEachGameObject([this, &checked] (uint32_t objectId1, GameObject* gameObject1) {
         forEachGameObject([this, objectId1, gameObject1, &checked] (uint32_t objectId2, GameObject* gameObject2) {
@@ -25,12 +27,12 @@ void ServerWorld::update(float elapsed) {
                 if (oid1 > oid2) {
                     std::swap(oid1, oid2);
                 }
-                const auto tuple = std::make_tuple(oid1, oid2);
-                if (checked.find(tuple) != checked.end()) {
+                const auto id = boost::str(boost::format("%1%,%2%") % oid1 % oid2);
+                if (checked.find(id) == checked.end()) {
                     if (gameObject1->checkCollision(gameObject2)) {
                         ERROR("{0} collides with {1}", oid1, oid2);
                     }
-                    checked.insert(tuple);
+                    checked.insert(id);
                 }
             }
         });
