ebfac13a35511941b500b00b502a1aa62ef0f5e9
Jan Deinhard <jan.deinhard@gmail.com>
2016-06-19 19:00:42 +0200

    add comments for BufferedQueue

--- src/BufferedQueue.h
+++ src/BufferedQueue.h
@@ -5,18 +5,42 @@
 #include "Packet.h"
 #include "Queue.h"
 
+/** A packet queue with its own pool.
+ */
 class BufferedQueue : public PacketSink {
 public:
+    /** Constructor
+
+        \param size the maximum size of the queue.
+     */
     explicit BufferedQueue(uint32_t size);
 
+    /** Push a packet to the pool.
+
+        \param packet a pointer to the packet that should be pushed on the pool.
+     */
     void push(Packet* packet) override;
 
+    /** Pop a packet from the pool.
+
+        \return a pointer to a packet.
+     */
     Packet* pop() override;
 
+    /** Add a packet to the end of the queue.
+
+        \param packet a pointer to the packet that should be added to the queue.
+     */
     void enqueue(Packet* packet) override;
 
+    /** Return and remove the first packet from the queue.
+
+        \return the first packet from the queue.
+     */
     Packet* dequeue() override;
 
+    /** Return the number of enqueued packets.
+     */
     uint32_t getNumEnqueued();
 
 private:
